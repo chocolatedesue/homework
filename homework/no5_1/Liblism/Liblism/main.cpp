61923a122207af4afbee545fb0b0d5949ba712c5
ccds <1356708169@qq.com>
2022-05-03 18:08:53 +0800

    update ui of admin

--- homework/no5_1/Liblism/Liblism/main.cpp
+++ homework/no5_1/Liblism/Liblism/main.cpp
@@ -33,7 +33,11 @@ Login login;
 
 // todo:用枚举替代常量，增加case语句的可读性
 enum Choice {
-	quit = 10,
+	INSERT = 1,
+	DROP,
+	QUERY,
+	UPDATE,
+	//quit = 10,
 };
 
 
@@ -51,7 +55,7 @@ Model commandline_parse(int argc, char* argv[], Library& library)
 	cmdline::parser parser;
 	parser.add<string>("type", 't', "the type of user，one of {Admin,Reader}", true, "", cmdline::oneof<string>("Admin", "Reader"));
 	parser.add<string>("name", 'n', "your name", true);
-	parser.add<int>("id",'i',"your id",true);
+	parser.add<int>("id", 'i', "your id", true);
 	parser.add<string>("passwd", 'p', "the passwd of admin if login as admin", false, "");
 	parser.parse_check(argc, argv);
 
@@ -99,37 +103,44 @@ Model commandline_parse(int argc, char* argv[], Library& library)
 class admin_opt {
 public:
 	void static insert_book(const book& src) {
+		//auto& tar = library.remain_book_list;
+		//auto get = lower_bound(tar.begin(), tar.end(), book("asd", src, 1));
 		bool flag = true;
 		for (auto& item : library.remain_book_list) {
 			if (item.name == src.name) {
-				auto temp = item;
+				/*auto temp = item;
 				temp.num++;
 				library.remain_book_list.erase(item);
 				library.remain_book_list.insert(temp);
+				flag = false;*/
+				cout << "已有此书，请使用更新操作" << endl;
 				flag = false;
 			}
 		}
 		if (flag) {
 			library.remain_book_list.insert(src);
+			cout << "书" << src.name << "已添加成功,id为" << src.id<<endl;
 		}
+
 		return;
 
 	}
-	void static drop_book(const book& src) {
+	void static drop_book(int id) {
 		auto& tar = library.remain_book_list;
-		auto get = lower_bound(tar.begin(), tar.end(), src);
-		if ((*get) == src) {
+		auto get = lower_bound(tar.begin(), tar.end(), book("asd", id, 1));
+		if (get != tar.end() && (*get).id == id) {
 			tar.erase(get);
-			cout << "操作成功，已删除" << src.name << endl;
+			string name = (*get).name;
+			cout << "操作成功，已删除" << name << endl;
 		}
 		else cout << "查无此书，删除错误" << endl;
 
 	}
-	
-	void static update_book(const book& src, int num) {
+
+	void static update_book(int id, int num) {
 		auto& tar = library.remain_book_list;
-		auto get = lower_bound(tar.begin(), tar.end(), src);
-		if ((*get) == src) {
+		auto get = lower_bound(tar.begin(), tar.end(), book("_",id,1));
+		if (get != tar.end() && (*get).id == id) {
 			tar.erase(get);
 			auto temp = *get;
 			if (temp.num + num <= 0) {
@@ -138,7 +149,7 @@ public:
 			else {
 				temp.num += num;
 				tar.insert(temp);
-				cout << "更新成功,"<<temp.name<<"变更"<<num<<"本" << endl;
+				cout << "更新成功," << temp.name << "变更" << num << "本" << endl;
 			}
 			//cout << "操作成功，已删除" << src.name << endl;
 		}
@@ -150,8 +161,8 @@ public:
 
 
 int main(int argc, char* argv[]) {
-	
-	
+
+
 #ifndef _DEVELOP
 	Model model = commandline_parse(argc, argv, library);
 #endif // _DEVELOP
@@ -171,12 +182,43 @@ int main(int argc, char* argv[]) {
 			cout << "请选择操作" << "\n"
 				<< "1. 增加图书" << "\n"
 				<< "2. 删除图书" << "\n"
-				<< "3. 修改图书" << "\n";
-			int opt; 
+				<< "3. 查询图书" << "\n"
+				<< "4. 修改图书" << "\n";
+			int opt;
 			scanf("%d", &opt);
 			switch (opt)
 			{
-				
+
+			case INSERT:
+			{
+				cout << "请输入要增添的书名和增添的数目" << endl;
+				string name; int num;
+				cin >> name >> num;
+				book temp(name, 1, num);
+				admin_opt::insert_book(temp);
+				break;
+			}
+			case DROP:
+			{
+				cout << "请输入要删除的书的id" << endl;
+				int id;
+				cin >> id;
+				admin_opt::drop_book(id);
+			}
+
+			case QUERY: 
+			{
+				;
+			}
+			case UPDATE:
+			{
+				cout << "请输入要变更的书的id和数目" << endl;
+				int id, num;
+				cin >> id >> num;
+				admin_opt::update_book(id, num);
+
+			}
+
 			default:
 				break;
 			}
@@ -184,7 +226,7 @@ int main(int argc, char* argv[]) {
 
 		}
 
-		
+
 	}
 
 
